50b8246ec5baf82ba69312a81b5db7156707ea3d
Ethan Joseph Kamus <ethanjpkamus@csu.fullerton.edu>
2019-10-23 12:25:55 -0700

    fill.cpp

--- fill.cpp
+++ fill.cpp
@@ -9,23 +9,22 @@ extern "C" int fill(int * arr[], int size);
 int fill(int * arr[], int size){
 
 	int count = 0;
-	int temp = 0;
+	int * temp = 0;
 
 	cout << "Input the integers you want. "
 	     << "Enter cntl+D when you are done" << endl;
 
 
-	while(getline(cin,temp)){
+	while(count != size){
 
-		if(count == size){
-			cout << "You have reached the maximum"
-			     << "amount of integers (10)" << endl;
-			break;
-		}
-
+		cin >> temp;
 		arr[count] = temp;
 		count++;
+		
 	}
+
+	cout << "You have reached the maximum amount of integers (10)" << endl;
+
 	return count;
 
 }
